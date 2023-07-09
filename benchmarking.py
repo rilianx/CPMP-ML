786697b3ff5b9abc9c5a25973837c4ba179a63be
Benjamín Robles <benjamin.robles.a@mail.pucv.cl>
2023-07-09 13:48:39 -0400

    benchmarking

--- benchmarking.py
+++ benchmarking.py
@@ -1,111 +1,130 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
-
-
-from imgui.integrations.glfw import GlfwRenderer
-import OpenGL.GL as gl
-import glfw
-import imgui
+import gi
+gi.require_version('Gtk', '3.0')
+from gi.repository import Gtk, GLib
 import sys
+from cpmp_ml import create_model, generate_data
+import numpy as np
 
 
-def callback(window, width, height):
-    p = glfw.get_window_user_pointer(window)
-    print(f"{width}x{height}")
-    gl.glViewport(0, 0, width, height)
-    p.impl.resize_callback(window, width, height)
-    p.width = width
-    p.height = height
-    # p.impl.io.display_size.x = width
-    # p.impl.io.display_size.y = height
-class GUI:
-    window = None
+with open('lol.xml', 'r') as file:
+    xml = file.read()
+
+@Gtk.Template(string=xml)
+class Benchmarking(Gtk.Box):
 
 
+    __gtype_name__ = "benchmarking"
+
+    input_model_btn = Gtk.Template.Child()
+    stack_count = Gtk.Template.Child()
+
+    model = None
+    S=5;
+    H=3;
+    N=10;
+    ss=100
+
+
+    @Gtk.Template.Callback()
+    def input_model_clicked(self, *args):
+        print("Select your input model")
+        f=Benchmarking.get_file()
+        if f:
+            self.input_model_btn.set_label(f.split("/")[-1])
+            self.model = create_model(self.S, self.H)
+        else: print("Error file:", f)
+        pass
+
+    @Gtk.Template.Callback()
+    def test_model_clicked(self, *args):
+        if  self.model == None:
+            Gtk.MessageDialog(parent = None, flags = 0,
+                              text = "Error: You must load a model first").run()
+        else:
+            x_test, y_test = generate_data(sample_size=self.ss, S=self.S, H=self.H, N=self.N)
+            test_loss, test_acc = self.model.evaluate(np.array(x_test), np.array(y_test))
+            print("test_loss: ", test_loss)
+            print("test_acc: ", test_acc)
+        pass
+        
+
+
+    @Gtk.Template.Callback()
+    def stack_count_value_changed(self, spin_button, *args):
+        self.S = spin_button.get_value_as_int()
+        print("New stack count:", self.S)
+        return 0
+
+    @Gtk.Template.Callback()
+    def stack_height_value_changed(self, spin_button, *args):
+        self.H = spin_button.get_value_as_int()
+        print("New stack height:", self.H)
+        return 0
+
+    @Gtk.Template.Callback()
+    def container_count_value_changed(self, spin_button, *args):
+        self.N = spin_button.get_value_as_int()
+        print("New container count:", self.N)
+        return 0
+
+    @Gtk.Template.Callback()
+    def sample_size_input_changed(self, spin_button, *args):
+        self.ss = spin_button.get_value_as_int()
+        print("New random sample size:", self.ss)
+        return 0
+
+
+
+
+
+    def get_file():
+        dialog = Gtk.FileChooserDialog(
+                title="Select a file",
+                parent=None,
+                action=Gtk.FileChooserAction.OPEN,
+                buttons=(
+                    Gtk.STOCK_CANCEL,
+                    Gtk.ResponseType.CANCEL,
+                    Gtk.STOCK_OPEN,
+                    Gtk.ResponseType.OK
+                )
+            )
+
+        response = dialog.run()
+        if response == Gtk.ResponseType.OK:
+            selected_file = dialog.get_filename()
+            dialog.destroy()
+            print("Selected file:", selected_file)
+            return selected_file
+        return None
+
+class Window(Gtk.Window):
     def __init__(self):
-        s=self
-        imgui.create_context()
-        s.window = self.impl_glfw_init()
-        s.impl = GlfwRenderer(s.window)
-        s.width, s.height = 1, 1
-    def __del__(self):
-        s=self
-        s.impl.shutdown()
-        glfw.terminate()
-
-
-    def loop(self):
-        s=self
-        while not glfw.window_should_close(s.window):
-            glfw.poll_events()
-            s.impl.process_inputs()
-
-            imgui.new_frame()
-
-            if imgui.begin_main_menu_bar():
-                if imgui.begin_menu("File", True):
-
-                    clicked_quit, selected_quit = imgui.menu_item(
-                        "Quit", "Cmd+Q", False, True
-                    )
-
-                    if clicked_quit:
-                        sys.exit(0)
-
-                    imgui.end_menu()
-                imgui.end_main_menu_bar()
-
-
-                imgui.set_next_window_position(0, 17)
-                imgui.set_next_window_size_constraints(
-                        (s.width, s.height),
-                        (s.width, s.height),
-                    )
-                imgui.begin("Custom window", True, flags = imgui.WINDOW_NO_TITLE_BAR)
-                imgui.text("Bar")
-                imgui.text_ansi("B\033[31marA\033[mnsi ")
-                imgui.text_ansi_colored("Eg\033[31mgAn\033[msi ", 0.2, 1.0, 0.0)
-                imgui.extra.text_ansi_colored("Eggs", 0.2, 1.0, 0.0)
-                imgui.end()
-
-            # imgui.show_test_window()
-
-            gl.glClearColor(0.0, 0.0, 0.0, 0)
-            gl.glClear(gl.GL_COLOR_BUFFER_BIT)
-
-            imgui.render()
-            s.impl.render(imgui.get_draw_data())
-            glfw.swap_buffers(s.window)
+        super().__init__(title = "Benchmarking")
+        self.foo = Benchmarking()
+        self.add(self.foo)
 
 
 
-    def impl_glfw_init(self):
-        width, height = 1280, 720
-        window_name = "minimal ImGui/GLFW3 example"
-
-        if not glfw.init():
-            print("Could not initialize OpenGL context")
-            sys.exit(1)
-
-        # OS X supports only forward-compatible core profiles from 3.2
-        glfw.window_hint(glfw.CONTEXT_VERSION_MAJOR, 3)
-        glfw.window_hint(glfw.CONTEXT_VERSION_MINOR, 3)
-        glfw.window_hint(glfw.OPENGL_PROFILE, glfw.OPENGL_CORE_PROFILE)
-        glfw.window_hint(glfw.TRANSPARENT_FRAMEBUFFER, gl.GL_TRUE)
-
-        glfw.window_hint(glfw.OPENGL_FORWARD_COMPAT, gl.GL_TRUE)
-
-        # Create a windowed mode window and its OpenGL context
-        window = glfw.create_window(int(width), int(height), window_name, None, None)
-        glfw.make_context_current(window)
-        glfw.set_window_user_pointer(window, self)
+print(f'Using GTK {Gtk.MAJOR_VERSION}.{Gtk.MINOR_VERSION}.{Gtk.MICRO_VERSION}')
 
 
-        if not window:
-            glfw.terminate()
-            print("Could not initialize Window")
-            sys.exit(1)
-        glfw.set_framebuffer_size_callback(window, callback)
+class MyApplication(Gtk.Application):
+    def __init__(self):
+        super().__init__(application_id="com.example.MyGtkApplication")
+        GLib.set_application_name('My Gtk Application')
 
-        return window
+    def do_activate(self):
+        win = Window()
+        win.connect("destroy", Gtk.main_quit)
+        win.show_all()
+        Gtk.main()
+        
+
+
+
+app = MyApplication()
+exit_status = app.run(sys.argv)
+sys.exit(exit_status)
