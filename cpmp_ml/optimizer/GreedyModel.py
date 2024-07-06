d677a12973678c19c09a8ec24d53f479d6e5a877
Thomas Molina <thomas.molina.s@mail.pucv.cl>
2024-07-05 20:49:18 -0400

    GreedyModel

--- cpmp_ml/optimizer/GreedyModel.py
+++ cpmp_ml/optimizer/GreedyModel.py
@@ -1,7 +1,47 @@
 from cpmp_ml.optimizer import OptimizerStrategy
 from cpmp_ml.utils import Layout
+from cpmp_ml.utils.adapters import DataAdapter
+from keras.models import Model
 import numpy as np
 
 class GreedyModel(OptimizerStrategy):
-    def solve(lays: np.ndarray[Layout]):
-        pass
+
+    def __init__(self, model:Model = None, 
+                 max_steps: int = None,
+                 data_adapter: DataAdapter = None):
+        if model is None or max_steps is None or data_adapter is None:
+            return ValueError("Some parameter was not given by argument.")
+        self.__model = model
+        self.__max_steps = max_steps
+        self.__data_adapter = data_adapter
+
+    def solve(self, lays: np.ndarray[Layout]):
+        costs = -np.ones(lays.shape[0])
+
+        for steps in range(self.__max_steps):
+            x = self.__get_valid_data(steps, costs, lays)
+            if x.shape[0] == 0:break
+            actions = self.__model.predict(x, verbose=False)[0]
+            self.__update_cost(actions, costs, lays)
+
+        return costs
+
+    def __get_valid_data(self, steps:int, costs:np.ndarray, lays:np.ndarray[Layout]) -> np.ndarray:
+        x = np.empty((0, *self.__data_adapter.get_ann_state(lays[0]).shape))
+        for i in range(lays.shape[0]):
+            if lays[i].unsorted_stacks==0: 
+                if costs[i] ==-1: costs[i]=steps
+                continue
+        ann_state = self.__data_adapter.get_ann_state(lays[i])
+        
+        x = np.vstack([x, ann_state[np.newaxis, :]])
+        return x
+    
+    def __update_cost(self, actions, costs:np.ndarray, lays:np.ndarray[Layout]) -> None:
+        k = 0
+        for i in range(lays.shape[0]):
+            if costs[i] != -1: continue
+            act = np.argmax(actions[k])
+            move = self.__data_adapter.get_move(act)
+            lays[i].move(move)
+            k+=1
--- cpmp_ml/optimizer/OptimizerStrategy.py
+++ cpmp_ml/optimizer/OptimizerStrategy.py
@@ -6,5 +6,5 @@ from numpy import ndarray
 class OptimizerStrategy(ABC):
     
     @abstractmethod
-    def solve(lays: ndarray[Layout]):
+    def solve(self, lays: ndarray[Layout]):
         pass
