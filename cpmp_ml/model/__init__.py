28a6fe25e7e7dcddbd7881dbce516adafcf2fdfc
Thomas Molina <thomas.molina.s@mail.pucv.cl>
2024-07-10 18:48:41 -0400

    Add declarations in the module

---
+++ cpmp_ml/model/__init__.py
@@ -0,0 +1,4 @@
+from cpmp_ml.model.functions import load_cpmp_model
+from cpmp_ml.model.functions import create_cpmp_model
+from cpmp_ml.model.functions import generate_model
+from cpmp_ml.model.functions import generate_model2
