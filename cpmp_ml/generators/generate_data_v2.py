f1bea0d6476b23fab3eb0e8026b3c76bd3b3bc88
Slinking196 <matias.bugueno.b@mail.pucv.cl>
2024-08-26 09:20:39 -0400

    fix bug with generate_data_v2

--- cpmp_ml/generators/generate_data_v2.py
+++ cpmp_ml/generators/generate_data_v2.py
@@ -22,7 +22,6 @@ def generate_steps_state(lay: Layout,
         y_ = generate_y(temp_lay, p_cost[0], optimizer, max_steps= max_steps)
 
         if y_ is None and len(lays) == 0: return None, None
-        elif y_ is None and len(lays) != 0: return lays, labels
 
         labels.append(y_)
         lays.append(adapter.get_ann_state(lay))
