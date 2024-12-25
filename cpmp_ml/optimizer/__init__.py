68f363be994a32b926351e669406e295cbc2ab15
slinking196 <matias.bugueno.b@mail.pucv.cl>
2024-12-25 14:12:42 -0300

    change libraries name in imports

--- cpmp_ml/optimizer/__init__.py
+++ cpmp_ml/optimizer/__init__.py
@@ -1,6 +1,4 @@
-"""from cpmp_ml.optimizer.OptimizerStrategy import OptimizerStrategy
+from cpmp_ml.optimizer.OptimizerStrategy import OptimizerStrategy
 from cpmp_ml.optimizer.GreedyV1 import GreedyV1
 from cpmp_ml.optimizer.GreedyV2 import GreedyV2
-from cpmp_ml.optimizer.GreedyModel import GreedyModel"""
-
-__all__ = ['OptimizerStrategy', 'GreedyV1', 'GreedyV2', 'GreedyModel']
+from cpmp_ml.optimizer.GreedyModel import GreedyModel
