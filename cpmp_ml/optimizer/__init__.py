811802775527e04fcdec51a2275c8abe62b3311b
slinking196 <matias.bugueno.b@mail.pucv.cl>
2024-12-25 04:49:36 -0300

    change __init__.py to add __all__ variable

--- cpmp_ml/optimizer/__init__.py
+++ cpmp_ml/optimizer/__init__.py
@@ -1,4 +1,19 @@
-from cpmp_ml.optimizer.OptimizerStrategy import OptimizerStrategy
-from cpmp_ml.optimizer.GreedyV1 import GreedyV1
-from cpmp_ml.optimizer.GreedyV2 import GreedyV2
-from cpmp_ml.optimizer.GreedyModel import GreedyModel
+__all__ = ["OptimizerStrategy", "GreedyV1", "GreedyV2", "GreedyModel"]
+
+_importables = {
+    "OptimizerStrategy": "cpmp_ml.optimizer.OptimizerStrategy",
+    "GreedyV1": "cpmp_ml.optimizer.GreedyV1",
+    "GreedyV2": "cpmp_ml.optimizer.GreedyModel",
+    "GreedyModel": "cpmp_ml.optimizer.GreedyModel"
+}
+
+def __getattr__(name):
+    """Carga los objetos bajo demanda."""
+    if name in _importables:
+        modulo = __import__(f".{_importables[name]}", globals(), locals(), [name])
+        return getattr(modulo, name)
+    raise AttributeError(f"module {__name__} has no attribute {name}")
+
+def __dir__():
+    """Define los nombres accesibles en el paquete."""
+    return list(globals().keys()) + list(_importables.keys())
