2012e135a78a0e78faff8be6472b484b30409959
slinking196 <matias.bugueno.b@mail.pucv.cl>
2024-12-25 04:51:25 -0300

    change __init__.py to add __all__ variable

--- cpmp_ml/optimizer/__init__.py
+++ cpmp_ml/optimizer/__init__.py
@@ -1,10 +1,10 @@
 __all__ = ["OptimizerStrategy", "GreedyV1", "GreedyV2", "GreedyModel"]
 
 _importables = {
-    "OptimizerStrategy": "cpmp_ml.optimizer.OptimizerStrategy",
-    "GreedyV1": "cpmp_ml.optimizer.GreedyV1",
-    "GreedyV2": "cpmp_ml.optimizer.GreedyModel",
-    "GreedyModel": "cpmp_ml.optimizer.GreedyModel"
+    "OptimizerStrategy": "cpmp_ml.optimizer",
+    "GreedyV1": "cpmp_ml.optimizer",
+    "GreedyV2": "cpmp_ml.optimizer",
+    "GreedyModel": "cpmp_ml.optimizer"
 }
 
 def __getattr__(name):
