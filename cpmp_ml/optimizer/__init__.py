e6c267abe10b6ebbd8cde6bda44cc6084f944bff
slinking196 <matias.bugueno.b@mail.pucv.cl>
2024-12-25 14:00:21 -0300

    change __init__.py to add __all__ variable

--- cpmp_ml/optimizer/__init__.py
+++ cpmp_ml/optimizer/__init__.py
@@ -1,23 +1,6 @@
-import importlib
+"""from cpmp_ml.optimizer.OptimizerStrategy import OptimizerStrategy
+from cpmp_ml.optimizer.GreedyV1 import GreedyV1
+from cpmp_ml.optimizer.GreedyV2 import GreedyV2
+from cpmp_ml.optimizer.GreedyModel import GreedyModel"""
 
-__all__ = ["OptimizerStrategy", "GreedyV1", "GreedyV2", "GreedyModel"]
-
-_importables = {
-    "OptimizerStrategy": "cpmp_ml.optimizer.OptimizerStrategy",
-    "GreedyV1": "cpmp_ml.optimizer.GreedyV1",
-    "GreedyV2": "cpmp_ml.optimizer.GreedyV2",
-    "GreedyModel": "cpmp_ml.optimizer.GreedyModel",
-}
-
-def __getattr__(name):
-    """Carga los objetos bajo demanda."""
-    if name in _importables:
-        module_name = _importables[name]
-        module = importlib.import_module(module_name)  # Importa el módulo usando importlib
-        return getattr(module, name)  # Obtiene el atributo del módulo
-    raise AttributeError(f"module {__name__} has no attribute {name}")
-
-def __dir__():
-    """Define los nombres accesibles en el paquete."""
-    return list(globals().keys()) + list(_importables.keys())
-
+__all__ = ['OptimizerStrategy', 'GreedyV1', 'GreedyV2', 'GreedyModel']
